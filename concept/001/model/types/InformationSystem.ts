d8a5e94efc59dcb21db34645917b8cba9ebd2489
David Válek <david.valek17@gmail.com>
2025-07-07 20:35:36 +0200

    Remove redundant method for printing infsys dirs

--- concept/001/model/types/InformationSystem.ts
+++ concept/001/model/types/InformationSystem.ts
@@ -6,18 +6,6 @@ class InfomationSystem {
     public name: string,
     public description: string,
 
-    public participants: Array<Participant>
   ) {}
 
-  public async getParticipantsFromJson(jsonPath: string): Promise<Array<Participant>> {
-    try {
-      const response = await fetch(jsonPath);
-      if (!response.ok) throw new Error('Network response was not ok');
-      const participants = await response.json();
-      return participants as Array<Participant>;
-    } catch (error) {
-      console.error('Error fetching participants JSON:', error);
-      return [];
-    }
-  }
 }
