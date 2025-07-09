62feb653c4fb39b0ff3b4e7adfaf0f3c975c6347
David Válek <david.valek17@gmail.com>
2025-07-09 11:41:21 +0200

    Create meal class

---
+++ concept/001/model/types/Meal.ts
@@ -0,0 +1,19 @@
+import type { TableEntity } from './TableEntity';
+
+export class Meal implements TableEntity {
+    constructor(
+        public id: number,
+        public name: string,
+        public whenServed: string,
+        public alergens: string[] = [],
+    ) {}
+
+    static fromJSON(json: any): Meal[] {
+        return json.map((item: any) => new Meal(
+            item.id,
+            item.name,
+            item.whenServed,
+            item.alergens || []
+        ));
+    }
+}
