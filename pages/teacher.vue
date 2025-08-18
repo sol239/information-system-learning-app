bc02a3435e559e5e433aa2c59ad1bde31589b77e
David Válek <david.valek17@gmail.com>
2025-08-18 10:35:43 +0200

    Update teacher page colors

--- pages/teacher.vue
+++ pages/teacher.vue
@@ -9,16 +9,16 @@
         <USeparator />
 
         <div class="mb-2 rounded-xl shadow p-6">
-            <h2 class="text-2xl font-bold mb-4 text-[#05df72]">{{ $t('solution_types_title') }}</h2>
-            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#05df72]">{{ $t('solution_1_title') }}</h3>
+            <h2 class="text-2xl font-bold mb-4 text-[#7c86ff]">{{ $t('solution_types_title') }}</h2>
+            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#9ca3ff]">{{ $t('solution_1_title') }}</h3>
             <p class="mb-4">
                 {{ $t('solution_1_desc') }}
             </p>
-            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#05df72]">{{ $t('solution_2_title') }}</h3>
+            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#9ca3ff]">{{ $t('solution_2_title') }}</h3>
             <p class="mb-4">
                 {{ $t('solution_2_desc') }}
             </p>
-            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#05df72]">{{ $t('solution_3_title') }}</h3>
+            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#9ca3ff]">{{ $t('solution_3_title') }}</h3>
             <p>
                 {{ $t('solution_3_desc') }}
             </p>
@@ -27,16 +27,16 @@
         <USeparator />
 
         <div class="rounded-xl shadow p-6">
-            <h2 class="text-2xl font-bold mb-4 text-[#05df72]">{{ $t('creation_types_title') }}</h2>
+            <h2 class="text-2xl font-bold mb-4 text-[#7c86ff]">{{ $t('creation_types_title') }}</h2>
             <p class="mb-4">
                 {{ $t('creation_intro') }}
                 <code class="bg-gray-800 px-1 rounded text-sm font-bold text-red-600">{{ $t('config_path') }}</code>
             </p>
-            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#05df72]">{{ $t('creation_1_title') }}</h3>
+            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#9ca3ff]">{{ $t('creation_1_title') }}</h3>
             <p class="mb-4">
                 {{ $t('creation_1_desc') }}
             </p>
-            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#05df72]">{{ $t('creation_2_title') }}</h3>
+            <h3 class="text-xl font-semibold mt-4 mb-2 text-[#9ca3ff]">{{ $t('creation_2_title') }}</h3>
             <p>
                 {{ $t('creation_2_desc') }}
             </p>
@@ -45,7 +45,7 @@
         <USeparator />
 
         <div class="rounded-xl shadow p-6">
-            <h2 class="text-2xl font-bold mb-4 text-[#05df72]">{{ $t('how_to_title') }}</h2>
+            <h2 class="text-2xl font-bold mb-4 text-[#7c86ff]">{{ $t('how_to_title') }}</h2>
             <p class="mb-4">
                 {{ $t('how_to_desc') }}
             </p>
