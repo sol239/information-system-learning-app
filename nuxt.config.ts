4a35fa1b24b2987178df9b71d8ac21b8458b263e
David Válek <david.valek17@gmail.com>
2025-09-04 14:02:19 +0200

    Add dependencies for testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxtjs/i18n', 'pinia-plugin-persistedstate/nuxt'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxtjs/i18n', 'pinia-plugin-persistedstate/nuxt', '@nuxt/test-utils/module'],
   piniaPluginPersistedstate: {
     storage: 'localStorage',
     debug: true,
